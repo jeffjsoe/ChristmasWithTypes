90e1bd32b8848bd8d02be646c32c82a2c4a86b94
Jeffrey Jose <jefjosecool@gmail.com>
2022-10-14 10:37:11 -0400

    Completed main method

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -14,11 +14,11 @@ namespace ChristmasWithTypes
             //Done set Santa's name to Kris Kringle
             xmas.Santa = "Kris Kringle";
 
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            //Done Insert 3 presents you would like for xmas.  They must be strings
+            xmas.Presents =  new string[3]{"Xbox", "Nintendo Switch", "Iphone x" };
 
-            //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            //Done Set the TreeHeight to 10
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
